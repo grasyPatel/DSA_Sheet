512e3a34b20364ab91957d5f4d52bf43130acbcf
grasypatel <gracepatel91@gmail.com>
2026-07-08 23:08:51 +0530

    BasicMath1

---
+++ BasicMaths/src/CountAllDigitsOfNumber.java
@@ -0,0 +1,13 @@
+
+public class CountAllDigitsOfNumber {
+    public int countDigits(int n){
+        int count=0;
+        while(n>0){
+            count++;
+            n=n/10;
+
+
+        }
+        return count;
+    }
+}
