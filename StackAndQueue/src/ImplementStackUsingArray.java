44ef1ee06af7d775f9523d973071ec96531c22f1
grasypatel <gracepatel91@gmail.com>
2026-07-31 15:34:13 +0530

    Stack Q1

--- StackAndQueue/src/ImplementStackUsingArray.java
+++ StackAndQueue/src/ImplementStackUsingArray.java
@@ -1,5 +1,50 @@
 public class ImplementStackUsingArray {
-    public void solution(int arr[]){
+    int size;
+    int arrStack[];
+    int index=-1;
+    public void solution(int capacity){
+        size=capacity;
+        arrStack=new int[size];
 
+
+
+
+    }
+    public void push(int x){
+        if(size-1==index) {
+            int arr[] = new int[size + 10];
+            size = size + 10;
+            for (int i = 0; i < arrStack.length; i++) {
+                arr[i] = arrStack[i];
+            }
+            arrStack=arr;
+
+        }
+        index++;
+        arrStack[index]=x;
+
+
+    }
+    public int  pop(){
+        if(!isEmpty()) {
+            int value = arrStack[index];
+            index--;
+            return value;
+        }
+        return -1;
+
+    }
+    public boolean isEmpty(){
+        return index==-1;
+    }
+    public int top(){
+        if(index==-1){
+            return -1;
+        }
+        return arrStack[index];
+    }
+
+    public int sizeOfStack(){
+        return index;
     }
 }
--- StackAndQueue/src/Main.java
+++ StackAndQueue/src/Main.java
@@ -1,9 +1,31 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
     public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n= sc.nextInt();
         ImplementStackUsingArray obj1=new ImplementStackUsingArray();
+        obj1.solution(n);
+        obj1.push(4);
+        obj1.push(6);
+        obj1.push(2);
+        obj1.push(9);
+        System.out.println("Top element "+obj1.top());
+        System.out.println("Deleting element: "+obj1.pop());
+        System.out.println("Top element "+obj1.top());
+        System.out.println(obj1.isEmpty());
+        System.out.println("Size of Stack "+obj1.sizeOfStack());
+
+        obj1.push(5);
+        obj1.push(0);
+        obj1.push(2);
+        System.out.println("Top Element: "+ obj1.top());
+        System.out.println("Size of Stack "+obj1.sizeOfStack());
+
+
 
 
     }
